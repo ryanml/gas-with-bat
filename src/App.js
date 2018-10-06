6245ef1282806afcf3eb21084327a0511776c97f
ryanml <rlanese@asu.edu>
2018-10-06 00:09:33 -0700

    Adding Transaction form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import TransactionForm from './TransactionForm'
+import './App.css'
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      formShown: false
+    }
+
+    this.toggleForm = this.toggleForm.bind(this)
+    this.mainContent = this.mainContent.bind(this)
+  }
+  
+  toggleForm () {
+    this.setState({
+      formShown: !this.state.formShown
+    })
+  }
+
+  submitTransaction () {
+    window.alert('Form Submit Action')
+  }
 
   metaMaskEnabled () {
     return typeof window.web3 !== 'undefined'
@@ -18,7 +39,20 @@ class App extends Component {
 
   mainContent () {
     return (
-      <p>MetaMask is enabled</p>
+      <div className='contentContainer'>
+        {
+          this.state.formShown
+          ? <TransactionForm 
+              onSubmit={this.submitTransaction}/>
+          : <div className='contentContainer'>
+              <p>MetaMask is enabled, welcome to bat-guano-dev!</p>
+              <p>Click the button below to get started</p>
+              <button onClick={this.toggleForm}>
+                Enable form
+              </button>
+            </div>
+        }
+      </div>
     )
   }
 
@@ -26,7 +60,7 @@ class App extends Component {
     const metaMask = this.metaMaskEnabled()
 
     return (
-      <div className="App">
+      <div className='app'>
         {
           metaMask
           ? this.mainContent()
---
+++ src/TransactionForm.js
@@ -0,0 +1,139 @@
+import React, { Component } from 'react'
+
+class Select extends Component {
+  constructor (props) {
+    super(props)
+
+    this.renderOption = this.renderOption.bind(this)
+  }
+
+  renderOption (option) {
+    return (
+      <option
+        key={option.value}
+        value={option.value}
+      >
+        {option.text}
+      </option>
+    )
+  }
+
+  render () {
+    const { title, name, options, onChange } = this.props
+
+    return (
+      <label className='form-label'>
+        <span>{title}: </span>
+        <select 
+          name={name}
+          onChange={e => onChange(name, e.target.value)}>
+            {options.map(option => this.renderOption(option))}
+        </select>
+      </label>
+    )
+  }
+}
+
+class TextInput extends Component {
+  render () {
+    const { title, name, placeholder, onChange } = this.props
+
+    return (
+      <label className='form-label'>
+        <span>{title}: </span>
+        <input 
+          type='text'
+          name={name}
+          onChange={e => onChange(name, e.target.value)}
+          placeholder={placeholder}/>
+      </label>
+    )
+  }
+}
+
+class SubmitButton extends Component {
+  render () {
+    const { title, onClick } = this.props
+
+    return (
+      <button onClick={onClick}>{title}</button>
+    )
+  }
+}
+
+export default class TransactionForm extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      amount: false,
+      recipient: false
+    }
+
+    this.validateForm = this.validateForm.bind(this)
+    this.updateFieldValue = this.updateFieldValue.bind(this)
+  }
+
+  validateForm () {
+    console.log(this.state)
+    if (!this.state.amount || !this.state.recipient) {
+      window.alert('One of the values was not filled out')
+      return
+    }
+
+    if (this.props.onSubmit) {
+      this.props.onSubmit()
+    }
+  }
+
+  updateFieldValue (field, value) {
+    let newState = {}
+    newState[field] = value
+    this.setState(newState)
+  }
+
+  get batAmounts () {
+    return [
+      {
+        value: '',
+        text: 'Select a value'
+      },
+      {
+        value: '10', 
+        text: '10'
+      },
+      {
+        value: '20', 
+        text: '20'
+      },
+      {
+        value: '30', 
+        text: '30'
+      },
+      {
+        value: '40', 
+        text: '40'
+      }
+    ]
+  }
+
+  render () {
+    return (
+      <>
+        <TextInput
+          title={'Recipient Address'}
+          name={'recipient'}
+          placeholder={'BAT Address'}
+          onChange={this.updateFieldValue}/>
+        <Select
+          title={'BAT'}
+          name={'amount'}
+          options={this.batAmounts}
+          onChange={this.updateFieldValue}/>
+        <SubmitButton
+          onClick={this.validateForm}
+          title={'Send Transaction'}/>
+      </>
+    )
+  }
+}
